371b6b40419b2fc94211ce2efb3da721ee82fc04
yyyyfan <931333877@qq.com>
2019-06-20 22:55:06 +0800

    work1 递归

---
+++ Week_01/id_33/4search/calculateMinimumHP/calculateMinimumHP.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+/**
+174. 地下城游戏
+*/
+
+func calculateMinimumHP(dungeon [][]int) int {
+	xLen, yLen := len(dungeon[0]), len(dungeon)
+
+}
+
+func main() {
+	arr := [][]int{{-2, -3, 3}, {-5, -10, 1}, {10, 30, -5}}
+	fmt.Println(calculateMinimumHP(arr))
+}
