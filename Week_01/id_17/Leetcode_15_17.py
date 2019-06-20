8829b618e4f7a25377158aedbc890d672d18c2b9
Geng Chang <imchangchang@gmail.com>
2019-06-21 00:32:13 +0800

    Update Leetcode_15_17.py

--- Week_01/id_17/Leetcode_15_17.py
+++ Week_01/id_17/Leetcode_15_17.py
@@ -1,32 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        n = len(nums)
+        
+        length = len(nums)
+        if length < 3:
+            return []
+        
+        nums.sort() 
         res = []
-        #print(nums)
-        for i in range(n):
+        
+        for i in range(0,length - 2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             left = i + 1
-            right = n - 1
+            right = length - 1
             while left < right:
-                cur_sum = nums[i] + nums[left] + nums[right]
-                if cur_sum == 0:
-                    tmp = [nums[i],nums[left],nums[right]]
-                    res.append(tmp)
+                s = nums[i] + nums[left] + nums[right]
+                #s = sum(tmp)
+                if s > 0:
+                    right -= 1
+                elif s < 0:
+                    left += 1
+                else:
+                    res.append([nums[i],nums[left],nums[right]])
                     while left < right and nums[left] == nums[left+1]:
                         left += 1
                     while left < right and nums[right] == nums[right-1]:
                         right -= 1
                     left += 1
                     right -= 1
-                elif cur_sum > 0:
-                    right -= 1
-                else:
-                    left += 1
         return res
-
-作者：powcai
-链接：https://leetcode-cn.com/problems/two-sum/solution/shuang-zhi-zhen-by-powcai-2/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+            
