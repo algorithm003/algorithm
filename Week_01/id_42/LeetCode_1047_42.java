0dcd7921589c78027dafd2e8bba5e6d7130edb23
wwdd22 <wwdd22@qq.com>
2019-06-23 15:35:04 +0800

    week_01

---
+++ Week_01/id_42/LeetCode_1047_42.java
@@ -0,0 +1,54 @@
+//给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。 
+//
+// 在 S 上反复执行重复项删除操作，直到无法继续删除。 
+//
+// 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。 
+//
+// 
+//
+// 示例： 
+//
+// 输入："abbaca"
+//输出："ca"
+//解释：
+//例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= S.length <= 20000 
+// S 仅由小写英文字母组成。 
+// 
+//
+
+import java.util.Stack;
+
+class Solution1047 {
+    public String removeDuplicates(String S) {
+        char[] s = S.toCharArray();
+        int len = s.length;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            if (stack.isEmpty() || s[i] != stack.peek()) {
+                stack.push(s[i]);
+            } else {
+                stack.pop();
+            }
+        }
+        StringBuilder str = new StringBuilder();
+        for (Character c : stack) {
+            str.append(c);
+        }
+        return stack.toString();
+
+    }
+
+    public static void main(String[] args) {
+        String s = "abbaca";
+        Solution1047 solution1047 = new Solution1047();
+        solution1047.removeDuplicates(s);
+    }
+}
---
+++ Week_01/id_42/LeetCode_15_42.java
@@ -0,0 +1,51 @@
+//给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+//
+//满足要求的三元组集合为：
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+// 
+//HHHHHHHHHH
+        }
+        Arrays.sort(nums);
+        HashSet<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    set.add(list);
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        Solution solution = new Solution();
+        solution.threeSum(nums);
+    }
+}
