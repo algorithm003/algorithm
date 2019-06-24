5fbb28bfd156ac5e400e1c34f5a68683e2937dfd
yyyyfan <Yf19951026>
2019-06-25 00:49:25 +0800

    work2 hashtable

---
+++ Week_02/id_33/1hashtable/LeetCode_1_33/LeetCode_1_33.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+/**
+1. 两数之和
+*/
+
+/**
+一次遍历，遍历的同时将元素插入map
+*/
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int, len(nums))
+	for k, v := range nums {
+		if val, ok := numMap[target-v]; ok {
+			return []int{val, k}
+		}
+		numMap[v] = k
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
---
+++ Week_02/id_33/1hashtable/LeetCode_3_33/LeetCode_3_33.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+/**
+3. 无重复字符的最长子串
+*/
+
+/**
+通过map记录出现过的字符位置
+设置两变量，一个存储当前最大长度，一个存储不重复起点
+当重复且不在重复起点前，设置新重复起点
+*/
+func lengthOfLongestSubstring(s string) int {
+	maps := make(map[rune]int)
+	count, index := 0, 0
+	for k, v := range s {
+		kOld, ok := maps[v]
+		if ok && kOld >= index {
+			index = kOld + 1
+		}
+		countTemp := k - index + 1
+		if countTemp > count {
+			count = countTemp
+		}
+		maps[v] = k
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("dvdf"))
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+	fmt.Println(lengthOfLongestSubstring("  "))
+}
