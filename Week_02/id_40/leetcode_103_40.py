ac0da16503e8f570da020a0f7662f370ce144dae
libinyl <1102109100@qq.com>
2019-06-27 01:03:01 +0800

    add problem 103

---
+++ Week_02/id_40/leetcode_103_40.py
@@ -0,0 +1,41 @@
+# https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/submissions/
+
+# 思路 1:迭代法
+# 其实与 102 几乎完全相同,只是判断了一下层级的奇偶性
+
+
+from queue import Queue
+from typing import List
+
+class Solution:
+    def _m(self, root: TreeNode):
+
+        ans = []
+        if not root:
+            return ans
+
+        q = Queue()
+        q.put((root, 1))
+
+        ans.append([root.val])
+
+        while not q.empty():
+            node, lev = q.get()
+            if len(ans) < lev:
+                ans.append([])
+
+            if lev > 1 and lev & 1 == 0:
+                ans[lev - 1].insert(0, node.val)
+            if lev > 1 and lev & 1 != 0:
+                ans[lev - 1].append(node.val)
+
+            if node.left:
+                q.put((node.left, lev + 1))
+            if node.right:
+                q.put((node.right, lev + 1))
+
+        return ans
+
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        return self._m(root)
+
