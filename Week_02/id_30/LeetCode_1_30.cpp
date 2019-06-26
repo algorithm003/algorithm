f73def0b910dd91a2a95f3d9b2454edc7c923714
unknown <10593@r2.com>
2019-06-26 15:27:01 +0800

    190626

---
+++ Week_02/id_30/LeetCode_1_30.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    /*
+    // 不需要额外空间，时间复杂度是O(n^2)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i+1; j < nums.size(); ++j) {
+                if (target == nums[i] + nums[j]) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }*/
+    // 空间复杂度O(n), 时间复杂度是O(n)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        map<int, int> dict;
+        for (int i = 0; i < nums.size(); ++i) {
+            int tmp = target - nums[i]; 
+            if (dict.find(tmp) != dict.end()) {
+                ans.push_back(i);
+                ans.push_back(dict[tmp]);
+                return ans;
+            }
+            else {
+                dict[nums[i]] = i;
+            }
+        }
+        return ans;
+    }
+};
---
+++ Week_02/id_30/LeetCode_242_30.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+     
+    // 空间复杂度O(n), 时间复杂度O(n)
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) 
+            return false;
+        // 将count 从map改成unordered_map提升了4ms
+        unordered_map<char, int> count;
+        for (int i = 0; i < s.length(); ++i) {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        for (auto & it : count) {
+            if (it.second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /*
+    // 空间复杂度为0 时间复杂度 O(nlogn), 
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }*/
+    
+};
