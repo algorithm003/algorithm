e0efbaeda07a64c438c57e7dbd7ab020bca5bedf
kkkkkk <0cool.c@gmail.com>
2019-06-29 17:08:21 +0800

    Create LeetCode_103_23.cpp

---
+++ Week_02/id_23/LeetCode_103_23.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        tranverse(root, res, 0);
+        return res;
+    }
+    
+    void tranverse(TreeNode *root, vector<vector<int>> &res, int level) {
+        if (root == NULL)
+            return;
+        
+        while (res.size() < level+1) {
+            res.push_back(vector<int>());
+        }
+        
+        (level & 1) ? (void)res[level].insert(res[level].begin(), root->val)
+                    : res[level].push_back(root->val);
+        
+        tranverse(root->left, res, level+1);
+        tranverse(root->right, res, level+1);
+    }
+};
