6b40fad94563a74e0557a689697494672a19ac52
zhongzuomin <1299076979@qq.com>
2019-06-30 22:41:27 +0800

    week_2_钟作敏_作业
    week_2_钟作敏_作业

--- Week_02/id_3/LeetCode_103_003.java
+++ Week_02/id_3/LeetCode_103_003.java
@@ -6,18 +6,26 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * @author zhongzuoming <zhongzuoming, 1299076979@qq.com>
- * @version v1.0
- * @Description baipao
- * @encoding UTF-8
- * @date 2019-06-30
- * @time 13:04
- * @修改记录 <pre>
- * 版本       修改人         修改时间         修改内容描述
- * --------------------------------------------------
- * <p>
- * --------------------------------------------------
- * </pre>
+ * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
+
+    给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）
+   例如：
+   给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+   返回锯齿形层次遍历如下：
+
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
  */
 public class LeetCode_103_003 {
 
