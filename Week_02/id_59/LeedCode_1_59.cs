05a2f6e542130571a97ccf43387cc197f3eb1986
杨永财 <youngwenicai@outlook.com>
2019-07-04 23:15:07 +0800

    1.补交作业（第一周+第二周）

---
+++ Week_02/id_59/LeedCode_1_59.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class LeedCode_1_59
+{
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = { 0, 0 };
+            int less = 0;
+            int temp = 0;
+            int currentIndex = 0;
+            bool isRight = false;
+            foreach (var item in nums)
+            {
+                less++;
+                for (int i = less; i < nums.Length; i++)
+                {
+                    temp = item + nums[i];
+                    if (temp == target)
+                    {
+                        result[0] = less - 1;
+                        result[1] = i;
+                        isRight = true;
+                    }
+                }
+                if (!isRight)
+                {
+                    currentIndex++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+}
---
+++ Week_02/id_59/LeedCode_3_59.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class LeedCode_3_59
+{
+    /// <summary>
+    /// 哈希表
+    /// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+    /// 示例 1:
+    /// 输入: "abcabcbb"
+    /// 输出: 3
+    /// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+    /// 来源：力扣（LeetCode）
+    /// 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+    /// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int LengthOfLongestSubstring(string s)
+    {
+        Stack<string> tempStrCollect = new Stack<string>();
+        tempStrCollect.Push("");
+        //Stack<string> strCollect = new Stack<string>();
+        StringBuilder tempStr = new StringBuilder();
+        for (int j = 0; j < s.Length; j++)
+        {
+            for (int i = j; i < s.Length; i++)
+            {
+                if (tempStr.ToString().IndexOf(s[i]) == -1)
+                {
+                    tempStr.Append(s[i]);
+                }
+                else
+                {
+                    //如何利用栈弹出与比较数值大小
+                    var tempStrCurrent = tempStr.ToString();
+                    //strCollect.Push(tempStrCurrent);
+                    var tempStrBefore = tempStrCollect.Pop();
+                    if (tempStrBefore.Length < tempStrCurrent.Length)
+                    {
+                        tempStrCollect.Push(tempStrCurrent);
+                    }
+                    else
+                    {
+                        tempStrCollect.Push(tempStrBefore);
+                    }
+                    tempStr.Clear();
+                    break;
+                }
+            }
+        }
+        var maxStr = tempStrCollect.Pop().Length;
+        if (0 == maxStr)
+        {
+            maxStr = tempStr.Length;
+        }
+
+        return maxStr;
+    }
+}
