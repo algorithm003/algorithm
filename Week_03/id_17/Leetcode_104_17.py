0569718ac0360b1f89e9c1a22e6af2a9ca1d0aba
Geng Chang <imchangchang@gmail.com>
2019-07-06 23:19:34 +0800

    Update Leetcode_104_17.py

--- Week_03/id_17/Leetcode_104_17.py
+++ Week_03/id_17/Leetcode_104_17.py
@@ -10,31 +10,36 @@
 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree  
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。  
 
-
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        
-        def travel_level(node_queue, level) -> int:
-            if node_queue == []:
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def bfs(queue,level):
+            if not queue:
                 return level-1
-            next_node_queue = []
-            for node in node_queue:
-                if node.left != None:
-                    next_node_queue.append(node.left)
-                if node.right != None:
-                    next_node_queue.append(node.right)
-            return travel_level(next_node_queue, level+1)
-        
-        if root is None:
+            next_queue=[]
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            return bfs(next_queue,level+1)
+        if not root:
             return 0
-        node_queue = []
-        node_queue.append(root)
-        
-        return travel_level(node_queue, 1)
+        queue=[root]
+        return bfs(queue,1)
+
+    之前做过一次，之前用的是DFS，这里尝试用BFS做了一遍
+    
+    时间复杂度：O（N），每个节点都访问了一次
+    空间复杂度：O（N），每一层都有一个queue用来保存该层的节点
+    
