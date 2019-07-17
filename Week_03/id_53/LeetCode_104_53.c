157e1cbdaf22b25a45d9e6eb273fe92b4ca9fa33
zturnxin <zturnxin@gmail.com>
2019-07-17 12:25:48 +0800

    week03

---
+++ Week_03/id_53/LeetCode_104_53.c
@@ -0,0 +1,51 @@
+/**
+ *
+ *
+ * Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+int maxDepth(struct TreeNode* root){
+    int maxlevel = -1;
+    if ( !root ) return 0;
+    if ( !root->left && !root->right ) return 1;
+    if ( root->left ) maxlevel = max(maxlevel, maxDepth(root->left)+1);
+    if ( root->right ) maxlevel = max(maxlevel, maxDepth(root->right)+1);
+    return maxlevel;
+}
+
